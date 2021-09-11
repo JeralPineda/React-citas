98598b5e9f495938e49aef796eccbad43a8b0b99
Jeral <jeral.cris@gmail.com>
2021-09-10 22:10:54 -0600

    Reinicial el formulario al crear la cita

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -50,6 +50,13 @@ export const Formulario = ({ crearCita }) => {
       crearCita(cita);
 
       // Reiniciar el form
+      actualizarCita({
+         mascota: '',
+         propietario: '',
+         fecha: '',
+         hora: '',
+         sintomas: '',
+      });
 
       console.log(cita);
    };
