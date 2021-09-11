f116b358e24948707ae883288671d6d439ec9751
Jeral <jeral.cris@gmail.com>
2021-09-10 20:19:54 -0600

    Agregando los campos del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,8 +2,59 @@ import React from 'react';
 
 export const Formulario = () => {
    return (
-      <div>
-         <h1>Desde el formulario</h1>
-      </div>
+      <>
+         <h2>Crear Cita</h2>
+
+         <form>
+            <label>Nombre Mascota</label>
+            <input
+               //
+               type='text'
+               name='mascota'
+               className='u-full-width'
+               placeholder='Nombre Mascota'
+            />
+
+            <label>Nombre Dueño</label>
+            <input
+               //
+               type='text'
+               name='propietario'
+               className='u-full-width'
+               placeholder='Nombre Dueño de la mascota'
+            />
+
+            <label>Fecha</label>
+            <input
+               //
+               type='date'
+               name='fecha'
+               className='u-full-width'
+            />
+
+            <label>Hora</label>
+            <input
+               //
+               type='time'
+               name='hora'
+               className='u-full-width'
+            />
+
+            <label>Sintomas</label>
+            <textarea
+               //
+               name='sintomas'
+               className='u-full-width'
+            ></textarea>
+
+            <button
+               //
+               type='submit'
+               className='u-full-width button-light'
+            >
+               Agregar Cita
+            </button>
+         </form>
+      </>
    );
 };
